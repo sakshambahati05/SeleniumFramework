7e239d35543d7b3b66fdce11d7cb39192c9cbc35
Saksham Bahati <saksham.bahati@gmail.com>
2019-01-13 11:46:45 +0530

    Selenium Automation Framework

---
+++ SeleniumAutomations/src/main/java/org/com/seleniumautomation/sa/App.java
@@ -0,0 +1,13 @@
+package org.com.seleniumautomation.sa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
