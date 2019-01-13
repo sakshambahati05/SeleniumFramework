2ec104abccaee43d6bb8c4cf6a8b6783da4053fb
Saksham Bahati <saksham.bahati@gmail.com>
2019-01-13 17:18:23 +0530

    This is for chrome and firefox

--- SeleniumAutomations/src/main/java/ChromeLaunch.java
+++ SeleniumAutomations/src/main/java/ChromeLaunch.java
@@ -1,17 +1,26 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ChromeLaunch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		System.setProperty("webdriver.chrome.driver","/Users/saksham.bahati/git/SeleniumAutomationFramework/SeleniumAutomations/chromedriver_mac64_2.45");
-		WebDriver driver=new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver",
+				"/Users/saksham.bahati/git/SeleniumAutomationFramework/SeleniumAutomations/chromedriver_mac64_2.45");
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
 		System.out.println(driver.getTitle());
 		driver.quit();
+
+		WebDriver driver1 = new FirefoxDriver();
+		System.setProperty("webdriver.gecko.driver",
+				"/Users/saksham.bahati/git/SeleniumAutomationFramework/SeleniumAutomations/geckodriver");
+		driver1.get("https://www.facebook.com");
+		System.out.println(driver1.getTitle());
+		driver1.quit();
+
 	}
 
 }
